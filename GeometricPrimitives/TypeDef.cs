123e98931095fe458ba66f0ac1e35dbacd1b8673
Felix Lilienthal <lilienthal_felix@web.de>
2021-01-01 21:47:36 +0100

    added typedef

---
+++ GeometricPrimitives/TypeDef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricPrimitives
+{
+    class TypeDef
+    {
+        public enum Signum { Positive, Negative, Zero, None } //"None" is for testing purposes, to make test fail that require return value but have not been implemented yet
+
+        public enum RelativePosition { Inside, Outside, Overlap, OnPoint, OnLine }
+
+        static class TypeDef
+        {
+        }
+    }
+}
