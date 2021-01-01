9a4b9bc8fe0482b9b41a9b4c82ecf73d0d3aa59e
Felix Lilienthal <lilienthal_felix@web.de>
2021-01-01 21:31:38 +0100

    Shape2D class added

---
+++ GeometricPrimitives/Shape2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricPrimitives
+{
+    public abstract class Shape2D
+    {
+        public abstract bool Equals(Shape2D shape, double epsilon = 1e-5);
+
+        public virtual double XMin { get; }
+        public virtual double XMax { get; }
+        public virtual double YMin { get; }
+        public virtual double YMax { get; }
+    }
+}
