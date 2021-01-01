4a969155e48edce683e67c971d6e47ca9e14c030
Felix Lilienthal <lilienthal_felix@web.de>
2021-01-01 22:09:25 +0100

    added Cricle2D class, no tests yet

---
+++ GeometricPrimitives/Circle2D.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricPrimitives
+{
+    public class Circle2D : Shape2D
+    {
+        public readonly Point2D Center;
+        public readonly double Radius;
+        public override double XMin
+        {
+            get
+            {
+                return Center.X - Radius;
+            }
+        }
+        public override double XMax
+        {
+            get
+            {
+                return Center.X + Radius;
+            }
+        }
+        public override double YMin
+        {
+            get
+            {
+                return Center.Y - Radius;
+            }
+        }
+        public override double YMax
+        {
+            get
+            {
+                return Center.Y + Radius;
+            }
+        }
+
+
+        public Circle2D(Point2D center, double radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public override bool Equals(Shape2D circle, double epsilon = 1e-5)
+        {
+            if (circle is Circle2D compare)
+            {
+                return (Center.Equals(compare.Center) && Utilities.SameValue(Radius, compare.Radius, epsilon));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
