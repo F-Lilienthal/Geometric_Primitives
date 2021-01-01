cf42039564765242499c11f46693163405314b42
Felix Lilienthal <lilienthal_felix@web.de>
2021-01-01 21:56:24 +0100

    added Ray2D and EmptyShape, both have virtually no code

---
+++ GeometricPrimitives/EmptyShape2D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricPrimitives
+{
+    public class EmptyShape2D : Shape2D
+    {
+        public override bool Equals(Shape2D emptyShape, double epsilon = 1e-5)
+        {
+            return this.GetType().Equals(emptyShape.GetType());
+        }
+    }
+}
---
+++ GeometricPrimitives/Ray2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometricPrimitives
+{
+    public class Ray2D : Shape2D
+    {
+        public readonly Point2D Position;
+        public readonly Point2D Direction;
+
+        public Ray2D(Point2D position, Point2D direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+
+        public override bool Equals(Shape2D ray, double epsilon = 1e-5)
+        {
+            if (ray is Ray2D)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
